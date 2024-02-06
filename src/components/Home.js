1746f1deb8dd453acee01e14a6206eebf0c8abf7
mcnentom <tommymcnen016@gmail.com>
2024-02-06 12:16:16 +0300

    my commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,18 @@ import './style.css';
 const Home = ({ onMovieClick }) => {
   const [movies, setMovies] = useState([]);
   const [searchInput, setSearchInput] = useState('');
+  const [isDarkTheme, setIsDarkTheme] = useState(
+    localStorage.getItem('isDarkTheme') === 'true'
+  );
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.body.classList.add('darkMode');
+    } else {
+      document.body.classList.remove('darkMode');
+    }
+    localStorage.setItem('isDarkTheme', isDarkTheme);
+  }, [isDarkTheme]);
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -27,31 +39,41 @@ const Home = ({ onMovieClick }) => {
     movie.title.toLowerCase().includes(searchInput.toLowerCase())
   );
 
+  const handleDarkMode = () => {
+    setIsDarkTheme((previousState) => !previousState);
+  };
+
   return (
     <div className='general'>
-      <h1 style={{margin :'10px 15px ',fontSize:'2.5rem'}}>Golango</h1>
-      <p style={{margin :'30px 15px ',fontSize:'1.5rem'}}>Watch your favourite movie's trailer</p>
-      <div className='searchBar'>
-        <input
-          type="text"
-          placeholder="Search by movie title"
-          value={searchInput}
-          onChange={handleSearch}
-          
-        />
+      <div className={isDarkTheme ? 'darkMode' : 'lightMode'}>
+        <button onClick={handleDarkMode} className='btn'>
+          {isDarkTheme ? 'light' : 'dark'}
+        </button>
+        <h1 style={{ margin: '10px 15px', fontSize: '2.5rem' }}>Golango</h1>
+        <p style={{ margin: '30px 15px', fontSize: '1.5rem' }}>
+          Watch your favourite movie's trailer
+        </p>
+        <div className='searchBar'>
+          <input
+            type='text'
+            placeholder='Search by movie title'
+            value={searchInput}
+            onChange={handleSearch}
+          />
+        </div>
+        <ul className='movies-list'>
+          {(filteredMovies ?? []).map((movie) => (
+            <li key={movie.id} className='movie-item'>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt={movie.title}
+                className='movie-poster'
+                onClick={() => onMovieClick(movie.id)}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
-      <ul className="movies-list">
-        {(filteredMovies ?? []).map((movie) => (
-          <li key={movie.id} className="movie-item">
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt={movie.title}
-              className="movie-poster"
-              onClick={() => onMovieClick(movie.id)}
-            />
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
